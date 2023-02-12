64bf286f39312eff38d1656c65a00d6c20bef011
Наталья <109668015+andre19071@users.noreply.github.com>
2023-02-12 19:42:18 +0700

    Add files via upload

---
+++ homework10/data.js
@@ -0,0 +1,38 @@
+ const dataInfo = `[
+    {
+    "id": "181913649",
+    "name": "Drake Hotline Bling",
+    "url": "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+    "width": "1200",
+    "height": "1200",
+    "box_count": "2",
+    "captions": "0"
+    },
+    {
+    "id": "87743020",
+    "name": "Two Buttons",
+    "url": "https://i.imgflip.com/1g8my4.jpg",
+    "width": "600",
+    "height": "908",
+    "box_count": "3",
+    "captions": "0"
+    },
+    {
+    "id": "112126428",
+    "name": "Distracted Boyfriend",
+    "url": "https://i.imgflip.com/1ur9b0.jpg",
+    "width": "1200",
+    "height": "800",
+    "box_count": "3",
+    "captions": "0"
+    },
+    {
+    "id": "112126428",
+    "name": "Distracted Boyfriend",
+    "url": "https://i.imgflip.com/1ur9b0.jpg",
+    "width": "1200",
+    "height": "800",
+    "box_count": "3",
+    "captions": "0"
+    }
+    ]`
