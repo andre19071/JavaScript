a8f8e7978b24cf97d096bd4d123b26da0b9f94db
Наталья <109668015+andre19071@users.noreply.github.com>
2023-02-16 14:52:03 +0700

    Add files via upload

---
+++ Seminar11/data.js
@@ -0,0 +1,45 @@
+const dataProducts = `[
+    {
+        "name": "MANGO  PEOPLE  T-SHIRT",
+        "price": "300",
+        "color":"red",
+        "size":"XL",
+        "quantity":"2",
+        "image":"./img/img1.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "price": "52",
+        "color":"black",
+        "size":"XL",
+        "quantity":"1",
+        "image":"./img/img2.png"
+    }
+]`
+ 
+const dataProductsDsc = `[
+    {
+        "name": "MOSCHINO CHEAP AND CHIC",
+        "description":"Compellingly actualize fully researched processes before proactive outsourcing. Progressively syndicate collaborative architectures before cutting-edge services. Completely visualize parallel core competencies rather than exceptional portals. "
+        "price": "561",
+        "image":"./img/imgDsc1.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. "
+        "price": "52",
+        "image":"./img/imgDsc2.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. "
+        "price": "52",
+        "image":"./img/imgDsc3.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. "
+        "price": "52",
+        "image":"./img/imgDsc4.png"
+    },
+]`
